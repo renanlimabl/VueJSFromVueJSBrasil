0f95896c969857db66b16a4a53ed482cd4df7ac8
Renan Lima <renanlimabl@gmail.com>
2021-02-14 18:40:28 -0300

    seta estado global

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,6 +8,7 @@ import ModalFactory from './components/ModalFactory'
 import { useRoute, useRouter } from 'vue-router'
 import { watch } from 'vue'
 import services from './services'
+import { setCurrentUser } from './store/user'
 
 export default {
   components: { ModalFactory },
@@ -24,7 +25,7 @@ export default {
         }
 
         const { data } = await services.users.getMe()
-        console.log(data)
+        setCurrentUser(data)
       }
     })
   }
--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import AuthService from './auth'
 import UsersService from './users'
 import router from '../router'
+import { setGlobalLoading } from '../store/global'
 
 const API_ENVS = {
   production: '',
@@ -13,20 +14,8 @@ const httpClient = axios.create({
   baseURL: API_ENVS.local
 })
 
-httpClient.interceptors.response.use((response) => response, (error) => {
-  const canThrowAnError = error.request.status === 0 || error.request.status === 500
-  if (canThrowAnError) {
-    throw new Error(error.message)
-  }
-
-  if (error.response.status === 401) {
-    router.push({ name: 'Home' })
-  }
-
-  return error
-})
-
 httpClient.interceptors.request.use(config => {
+  setGlobalLoading(true)
   const token = window.localStorage.getItem('token')
 
   if (token) {
@@ -34,6 +23,25 @@ httpClient.interceptors.request.use(config => {
   }
 
   return config
+})
+
+httpClient.interceptors.response.use((response) => {
+  setGlobalLoading(false)
+  return response
+}, (error) => {
+  const canThrowAnError = error.request.status === 0 || error.request.status === 500
+
+  if (canThrowAnError) {
+    setGlobalLoading(false)
+    throw new Error(error.message)
+  }
+
+  if (error.response.status === 401) {
+    router.push({ name: 'Home' })
+  }
+
+  setGlobalLoading(false)
+  return error
 })
 
 export default {
