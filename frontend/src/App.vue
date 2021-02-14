dee074e0185e47f462663de8fa8e0887cf949b3b
Renan Lima <renanlimabl@gmail.com>
2021-02-14 18:25:49 -0300

    add new services for users

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,8 +5,28 @@
 
 <script>
 import ModalFactory from './components/ModalFactory'
+import { useRoute, useRouter } from 'vue-router'
+import { watch } from 'vue'
+import services from './services'
 
 export default {
-  components: { ModalFactory }
+  components: { ModalFactory },
+  setup () {
+    const router = useRouter()
+    const route = useRoute()
+
+    watch(() => route.path, async () => {
+      if (route.meta.hasAuth) {
+        const token = window.localStorage.getItem('token')
+
+        if (!token) {
+          router.push({ name: 'Home' })
+        }
+
+        const { data } = await services.users.getMe()
+        console.log(data)
+      }
+    })
+  }
 }
 </script>
--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import AuthService from './auth'
+import UsersService from './users'
+import router from '../router'
 
 const API_ENVS = {
   production: '',
@@ -16,9 +18,25 @@ httpClient.interceptors.response.use((response) => response, (error) => {
   if (canThrowAnError) {
     throw new Error(error.message)
   }
+
+  if (error.response.status === 401) {
+    router.push({ name: 'Home' })
+  }
+
   return error
 })
 
+httpClient.interceptors.request.use(config => {
+  const token = window.localStorage.getItem('token')
+
+  if (token) {
+    config.headers.common.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
+
 export default {
-  auth: AuthService(httpClient)
+  auth: AuthService(httpClient),
+  users: UsersService(httpClient)
 }
